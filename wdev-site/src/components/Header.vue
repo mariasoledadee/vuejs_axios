6707f3eb9901c146f7d5d16f988eacf47a7dcf37
Maria Soledade <maryaferreira94@gmail.com>
2021-05-25 21:31:37 -0300

    Criando o componente Header.vue

---
+++ wdev-site/src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <nav class="container">
+      <a href="/">
+        <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg"
+         alt="logo WDEV" 
+         id="logo"
+        >
+      </a>
+
+      <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg"
+       alt="Abrir menu" 
+       id="menu-btn"
+      >
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  header{
+    background-color: var(--color-background-nav);
+    height: 60px;
+    width: 100%;
+  }
+  nav{
+    display: flex;
+  }
+  #logo{
+    width: 130px;
+  }
+  #menu-btn{
+    width: 30px;
+  }
+</style>
